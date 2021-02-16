ca4ebf5207c19c66cd1c9a53447b521740af760f
mkleijn <mkleijn@expertcollege.com>
2021-02-16 13:56:37 +0100

    Create common.js

---
+++ common.js
@@ -0,0 +1,17 @@
+const connOptions = {
+    hosts: {
+        domain: 'jitsi.expertcollege.com',
+        muc: 'conference.jitsi.expertcollege.com' // FIXME: use XEP-0030
+    },
+    bosh: '//jitsi.expertcollege.com/http-bind', // FIXME: use xep-0156 for that
+
+    // The name of client node advertised in XEP-0115 'c' stanza
+    clientNode: 'http://jitsi.org/jitsimeet'
+};
+
+const initOptions = {
+    disableAudioLevels: true
+};
+
+ JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.ERROR);
+ JitsiMeetJS.init(initOptions);
