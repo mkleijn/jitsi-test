13ae6e6aa4a2f25ba872d2c31c65e14fb2edd537
mkleijn <mkleijn@expertcollege.com>
2021-02-21 16:24:23 +0100

    Update common.js

--- common.js
+++ common.js
@@ -3,7 +3,7 @@ const connOptions = {
         domain: 'jitsi.expertcollege.com',
         muc: 'conference.jitsi.expertcollege.com' // FIXME: use XEP-0030
     },
-    //bosh: '//jitsi.expertcollege.com/http-bind', // FIXME: use xep-0156 for that
+    bosh: '//jitsi.expertcollege.com/http-bind', // FIXME: use xep-0156 for that
     websocket: 'wss://jitsi.expertcollege.com/xmpp-websocket',
     // The name of client node advertised in XEP-0115 'c' stanza
     clientNode: 'http://jitsi.org/jitsimeet'
